07c332fad6ab1dd510eb83fb1ea58f048b218e35
sscanni <steven.scanniello@snhu.edu>
2018-10-23 22:20:49 -0400

    Added  @app.route

--- endpoints.py
+++ endpoints.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__) 
 # Create the appropriate app.route functions. Test and see if they work
 
 #Make an app.route() decorator here for when the client sends the URI "/puppies"
-
+@app.route('/puppies', methods=['GET', 'POST'])
 def puppiesFunction():
-  return "Yes, puppies!"
+    if request.method == 'GET':
+       return "Yes, GET puppies!"
+    if request.method == 'POST':
+       return "Yes, POST puppies!"
   
  
 #Make another app.route() decorator here that takes in an integer named 'id' for when the client visits a URI like "/puppies/5"
-
+@app.route('/puppies/<int:id>', methods=['GET', 'POST'])
 def puppiesFunctionId(id):
-  return "This method will act on the puppy with id %s" % id
+  if request.method == 'GET':
+     return "This GET method will act on the puppy with id %s" % id
+  if request.method == 'POST':
+     return "This POST method will act on the puppy with id %s" % id
+
 
 if __name__ == '__main__':
     app.debug = True
