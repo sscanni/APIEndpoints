12ec0108a2938d39f6e5339ef30e52f4471f7e6d
sscanni <steven.scanniello@snhu.edu>
2018-10-24 21:00:36 -0400

    Added serialize decorator to models.py

--- models.py
+++ models.py
@@ -11,8 +11,16 @@ class Puppy(Base):
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
+    
     #Add add a decorator property to serialize data from the database
-
+    @property
+    def serialize(self):
+        #Returns object data in easily serializeable format
+        return {
+            'name' : self.name,
+            'id' : self.id,
+            'description' : self.description,
+        }
 
 
 engine = create_engine('sqlite:///puppies.db')
